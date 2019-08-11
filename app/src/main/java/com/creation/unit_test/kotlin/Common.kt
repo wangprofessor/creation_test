73ea8352546bf67270e9488d4499adaa4248c010
Wang Shouchao <wsc0520@163.com>
2019-08-11 08:01:26 +0800

    123

--- app/src/main/java/com/creation/unit_test/kotlin/Common.kt
+++ app/src/main/java/com/creation/unit_test/kotlin/Common.kt
@@ -1,57 +1,9 @@
 package com.creation.unit_test.kotlin
 
-import android.content.Context
-import android.support.v4.app.Fragment
-import android.view.View
-
 object CommonTest {
-    @Volatile private var enable = false
-    private lateinit var a: () -> Unit
-    private lateinit var b: () -> Unit
 
-    @JvmStatic
-    fun main(args: Array<String>) {
+}
 
-        val ob = Runnable {
-            test {
+fun main(args: Array<String>) {
 
-            }
-        }
-        ob.run()
-    }
-
-    fun eanable() {
-        enable = true
-        //
-        //
-    }
-
-    fun tryIfEnable(a: ()->Unit) {
-        if (enable) {
-            a()
-        }
-    }
-
-    fun test(cab: () -> Unit) {
-        System.out.println("measure_image")
-
-        tryIfEnable {
-            a.invoke()
-            b.invoke()
-        }
-
-        fun1(0, Runnable {})
-    }
-
-    private fun fun1(aa: Int, runnable: Runnable) {}
-
-    fun abc(a: Int = 0) {
-
-    }
-
-    class A(context: Context) : View(context) {
-        constructor(b: Int, context: Context): this(context) {
-
-        }
-    }
 }
